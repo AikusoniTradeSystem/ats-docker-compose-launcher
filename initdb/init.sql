13895c9296f09c71409993c42cda65d6c5109b18
HoonKi Kim <31752803+aikusoni@users.noreply.github.com>
2024-09-14 16:49:00 +0900

    [feature/user-db:] * user-db 추가

---
+++ initdb/init.sql
@@ -0,0 +1,29 @@
+-- ./initdb/init.sql
+
+CREATE TABLE IF NOT EXISTS USERS
+(
+    USERNAME VARCHAR(50) NOT NULL PRIMARY KEY,
+    PASSWORD VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS USER_ROLE
+(
+    USERNAME VARCHAR(50) NOT NULL,
+    ROLENAME VARCHAR(50) NOT NULL,
+    PRIMARY KEY (USERNAME, ROLENAME)
+);
+
+INSERT INTO USERS (USERNAME, PASSWORD)
+VALUES ('admin', '{bcrypt}$2a$10$G91Biiz2qomu/feju3BFNOyeM1hfrrdsARWW00hXi0SKPEMPLmKfW'), -- password is password123!
+       ('user1', '{bcrypt}$2a$10$G91Biiz2qomu/feju3BFNOyeM1hfrrdsARWW00hXi0SKPEMPLmKfW'), -- password is password123!
+       ('user2', '{bcrypt}$2a$10$G91Biiz2qomu/feju3BFNOyeM1hfrrdsARWW00hXi0SKPEMPLmKfW'); -- password is password123!
+
+INSERT INTO USER_ROLE (USERNAME, ROLENAME)
+VALUES ('admin', 'admin'),
+       ('admin', 'item-registration'),
+       ('user1', 'item-sell'),
+       ('user1', 'item-buy'),
+       ('user1', 'item-stat'),
+       ('user2', 'item-sell'),
+       ('user2', 'item-buy'),
+       ('user2', 'item-stat');
