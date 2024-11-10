1eb7eed532aef5a4db036e6e95749a69eaa9d634
HoonKi Kim <31752803+aikusoni@users.noreply.github.com>
2024-11-10 13:24:11 +0900

    pki-policy 추가, 볼트 syslog 추가 [renewal/script_seq_and_certs] * pki-policy를 init_vault.sh에서 생성하지 않고 있어서 pki-policy를 생성하도록 변경 * 모니터링 도구로 감사 로그를 전달 할 수 있도록 syslog 활성화

--- R12_20_0000_enable_syslog_audit_engine.sh
+++ R12_20_0000_enable_syslog_audit_engine.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # ==============================================
-# Script Name:	Enable Vault Secrets Engine - PKI
-# Description:	This script enables the PKI secrets engine in Vault.
+# Script Name:	Enable Vault Syslog Audit Engine
+# Description:	This script enables the syslog audit engine in Vault.
 # ==============================================
 # Reference:
-# HashiCorp Vault PKI Secrets Engine: Quick Start - Intermediate CA Setup
-# ( https://developer.hashicorp.com/vault/docs/secrets/pki/quick-start-intermediate-ca )
+# HashiCorp Vault Syslog Audit Backend
+# ( https://developer.hashicorp.com/vault/docs/audit/syslog )
 # ==============================================
 
 (
   source load_env.sh
   source load_function.sh
 
-  ENGINE_TYPE="secrets"
-  ENGINE_NAME="pki"
+  ENGINE_TYPE="audit"
+  ENGINE_NAME="syslog"
   VAULT_POLICY_TOKEN=$(awk -F'"' '/"client_token"/ {print $4}' "${VAULT_CREDENTIAL_INIT_PATH}/admin-policy.json")
 
   ./R12_BASE_00_enable_engine.sh \
@@ -22,5 +22,5 @@
     --engine_name="$ENGINE_NAME" \
     --vault_policy_token="$VAULT_POLICY_TOKEN"
 
-  exit_on_error "Failed to enable Vault Secrets Engine - PKI."
+  exit_on_error "Failed to enable Vault Audit Engine - Syslog."
 )
