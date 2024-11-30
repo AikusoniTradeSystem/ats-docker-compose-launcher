5cb71806bc3bc31758f0fef29b01b9a817409eec
HoonKi Kim <31752803+aikusoni@users.noreply.github.com>
2024-11-30 17:12:37 +0900

    user db를 서명하는 스크립트 추가 [feature/certs] user db를 서명하는 스크립트 추가 볼트에서 인증서에 서명할 수 있도록 extra_hosts에 bare_domain 추가

---
+++ CMN_generate_and_sign_server_key.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+# ==============================================
+# Script Name:  Generate and Sign Server Key
+# Description:	This script generates and signs the server key with the Vault PKI.
+# ==============================================
+
+if [ "$0" = "sh" ] || [ "$0" = "bash" ]; then
+  echo -e "Error: This script must be executed from another shell script."
+  exit 1
+fi
+
+(
+  source CMN_load_function.sh
+
+  TEMP_FILE_KEY="$$"
+  TEMP_DIR=$(create_temp_dir "${TEMP_FILE_KEY}")
+  VAULT_PKI_TOKEN=""
+  ACCESS_CA_CERT_PATH=""
+  BARE_HOST_NAME=""
+  SERVER_CONTAINER_NAME=""
+  SERVER_HOST_NAME=""
+  SERVER_PKI_ROLE_NAME=""
+
+  PRIVATE_KEY_OUTPUT_PATH=""
+  CSR_OUTPUT_PATH=""
+  CERTIFICATE_OUTPUT_PATH=""
+
+  # 명령행 인자를 처리하는 while 루프
+  while [[ "$#" -gt 0 ]]; do
+    case $1 in
+      --vault_pki_token=*) VAULT_PKI_TOKEN="${1#*=}"; shift ;;
+      --access_ca_cert_path=*) ACCESS_CA_CERT_PATH="${1#*=}"; shift ;;
+      --bare_host_name=*) BARE_HOST_NAME="${1#*=}"; shift ;;
+      --server_container_name=*) SERVER_CONTAINER_NAME="${1#*=}"; shift ;;
+      --server_host_name=*) SERVER_HOST_NAME="${1#*=}"; shift ;;
+      --server_pki_role_name=*) SERVER_PKI_ROLE_NAME="${1#*=}"; shift ;;
+      --private_key_output_path=*) PRIVATE_KEY_OUTPUT_PATH="${1#*=}"; shift ;;
+      --csr_output_path=*) CSR_OUTPUT_PATH="${1#*=}"; shift ;;
+      --certificate_output_path=*) CERTIFICATE_OUTPUT_PATH="${1#*=}"; shift ;;
+      *) echo -e "Unknown option: $1" >&2; exit 1 ;;
+    esac
+  done
+
+  log i "Generate and Sign Server Key"
+  log d "VAULT_PKI_TOKEN: ${VAULT_PKI_TOKEN}"
+  log d "ACCESS_CA_CERT_PATH: ${ACCESS_CA_CERT_PATH}"
+  log d "BARE_HOST_NAME: ${BARE_HOST_NAME}"
+  log d "SERVER_CONTAINER_NAME: ${SERVER_CONTAINER_NAME}"
+  log d "SERVER_HOST_NAME: ${SERVER_HOST_NAME}"
+  log d "SERVER_PKI_ROLE_NAME: ${SERVER_PKI_ROLE_NAME}"
+  log d "PRIVATE_KEY_OUTPUT_PATH: ${PRIVATE_KEY_OUTPUT_PATH}"
+  log d "CSR_OUTPUT_PATH: ${CSR_OUTPUT_PATH}"
+  log d "CERTIFICATE_OUTPUT_PATH: ${CERTIFICATE_OUTPUT_PATH}"
+
+  TEMP_PRIVATE_KEY_PATH="${TEMP_DIR}/private.key"
+  TEMP_CSR_PATH="${TEMP_DIR}/request.csr"
+  TEMP_CERTIFICATE_PATH="${TEMP_DIR}/certificate.crt"
+
+  # Generate PKI Role for current database
+  log d "Generating PKI Role for ${SERVER_PKI_ROLE_NAME}..."
+  try curl --cacert "$ACCESS_CA_CERT_PATH" \
+       --header "X-Vault-Token: ${VAULT_TOKEN}" \
+       --request POST \
+       --data '{
+            "allowed_domains": "'${BARE_HOST_NAME}'",
+            "allow_subdomains": true,
+            "allow_ip_sans": true,
+            "max_ttl": "720h"
+       }' \
+       "https://localhost:8200/v1/pki/roles/${SERVER_PKI_ROLE_NAME}"
+
+  # Generate the private key
+  log d "Generating private key... ${TEMP_PRIVATE_KEY_PATH}"
+  openssl genrsa -out "$TEMP_PRIVATE_KEY_PATH" 4096
+  exit_on_error "Failed to generate private key."
+
+  # Generate the request csr
+  log d "Generating request csr... ${TEMP_CSR_PATH}"
+  openssl req -new -key "$TEMP_PRIVATE_KEY_PATH" -out "$TEMP_CSR_PATH" \
+      -subj "/CN=${SERVER_HOST_NAME}" \
+      -reqexts SAN \
+      -config <(cat <<EOF
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = SAN
+prompt = no
+
+[req_distinguished_name]
+CN =${SERVER_HOST_NAME}
+
+[SAN]
+subjectAltName = DNS.1:${SERVER_HOST_NAME}, DNS.2:localhost, IP:127.0.0.1
+EOF
+)
+  exit_on_error "Failed to generate request csr."
+
+  # Sign the certificate
+  log d "Signing the certificate... ${TEMP_CERTIFICATE_PATH}"
+  try curl --cacert "$ACCESS_CA_CERT_PATH" \
+    --header "X-Vault-Token: ${VAULT_TOKEN}" \
+    --header "Content-Type: application/json" \
+    --request POST \
+    --data "$(jq -n --arg csr "$(cat $TEMP_CSR_PATH)" '{csr: $csr}')" \
+    "https://localhost:8200/v1/pki/sign/${SERVER_PKI_ROLE_NAME}" | jq -r '.data.certificate' > "$TEMP_CERTIFICATE_PATH"
+
+  # Copy the certificate to the output path
+  log d "Copying the certificate to the output path..."
+  try cp "$TEMP_PRIVATE_KEY_PATH" "$PRIVATE_KEY_OUTPUT_PATH"
+  try cp "$TEMP_CSR_PATH" "$CSR_OUTPUT_PATH"
+  try cp "$TEMP_CERTIFICATE_PATH" "$CERTIFICATE_OUTPUT_PATH"
+
+  # cleanup temp dirs
+  log i "Cleaning up temp dirs..."
+  print_temp_dirs ${TEMP_FILE_KEY}
+  cleanup_temp_dirs ${TEMP_FILE_KEY}
+)
--- R21_0000_update_userdb_tls_key.sh
+++ R21_0000_update_userdb_tls_key.sh
@@ -9,5 +9,40 @@
   source CMN_load_env.sh
   source CMN_load_function.sh
 
-  try docker exec -i "${USER_DB_CONTAINER_NAME}" mkdir -p "${USER_DB_CLIENT_CRYPTO_PATH}"
+  DOCKER_COMPOSE_FILE_NAME="20_docker-compose.db.yml"
+
+  TEMP_KEY="$$"
+  TEMP_DIR=$(create_temp_dir "${TEMP_KEY}")
+  VAULT_PKI_POLICY_TOKEN=$(awk -F'"' '/"client_token"/ {print $4}' "${VAULT_CREDENTIAL_INIT_PATH}/pki-policy.json")
+  ACCESS_CA_CERT_PATH="${INTER_CA2_PUBLIC_CHAIN_CERT_PATH}"
+  BARE_HOST_NAME="${BARE_HOST_NAME}"
+  SERVER_CONTAINER_NAME="${USER_DB_CONTAINER_NAME}"
+  SERVER_HOST_NAME="${USER_DB_HOST_NAME}"
+  SERVER_PKI_ROLE_NAME="${SERVER_CONTAINER_NAME}-role"
+  TEMP_PRIVATE_KEY_PATH="${TEMP_DIR}/private.key"
+  TEMP_CSR_PATH="${TEMP_DIR}/request.csr"
+  TEMP_CERTIFICATE_PATH="${TEMP_DIR}/certificate.crt"
+  CONTAINER_PRIVATE_KEY_PATH="${USER_DB_CERT_PATH_IN_CONTAINER}/server.key"
+  CONTAINER_CERTIFICATE_PATH="${USER_DB_CERT_PATH_IN_CONTAINER}/server.crt"
+
+  try ./CMN_generate_and_sign_server_key.sh \
+    --vault_pki_token="${VAULT_PKI_POLICY_TOKEN}" \
+    --access_ca_cert_path="${ACCESS_CA_CERT_PATH}" \
+    --bare_host_name="${BARE_HOST_NAME}" \
+    --server_container_name="${SERVER_CONTAINER_NAME}" \
+    --server_host_name="${SERVER_HOST_NAME}" \
+    --server_pki_role_name="${SERVER_PKI_ROLE_NAME}" \
+    --private_key_output_path="${TEMP_PRIVATE_KEY_PATH}" \
+    --csr_output_path="${TEMP_CSR_PATH}" \
+    --certificate_output_path="${TEMP_CERTIFICATE_PATH}"
+
+  try docker cp "${TEMP_PRIVATE_KEY_PATH}" "${SERVER_CONTAINER_NAME}":"${CONTAINER_PRIVATE_KEY_PATH}"
+  try docker cp "${TEMP_CERTIFICATE_PATH}" "${SERVER_CONTAINER_NAME}":"${CONTAINER_CERTIFICATE_PATH}"
+
+  # Restart the server container to apply the new SSL key
+  # TODO Rolling restart for production
+  try docker compose -f "${DOCKER_COMPOSE_FILE_NAME}" restart "${SERVER_CONTAINER_NAME}"
+
+  print_temp_dirs "${TEMP_KEY}"
+  cleanup_temp_dirs "${TEMP_KEY}"
 )
